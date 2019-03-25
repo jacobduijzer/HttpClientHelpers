6653b08e53ce640321c1ef49b625a602579063de
Jacob Duijzer <github@duijzer.com>
2019-03-25 08:55:01 +0100

    added new handler, yml file change

---
+++ src/HttpClientHelpers/HttpClientHelpers.Library.Tests/MessageHandlers/ExtraParameters/ExtraParametersHandlerShould.cs
@@ -0,0 +1,21 @@
+﻿using HttpClientHelpers.Library.MessageHandlers.ExtraParameters;
+using System.Collections.Generic;
+using Xunit;
+
+namespace HttpClientHelpers.Library.Tests.MessageHandlers.ExtraParameters
+{
+    public class ExtraParametersHandlerShould
+    {
+        private readonly Dictionary<string, string> _fakeDictionary;
+
+        public ExtraParametersHandlerShould() =>
+            _fakeDictionary = new Dictionary<string, string>
+            {
+                { "testparam", "testvalue" }
+            };
+
+        [Fact]
+        public void Construct() =>
+            new ExtraParametersHandler(_fakeDictionary).Should().BeOfType<ExtraParametersHandler>();
+    }
+}
---
+++ src/HttpClientHelpers/HttpClientHelpers.Library/MessageHandlers/ExtraParameters/ExtraParametersHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HttpClientHelpers.Library.MessageHandlers.ExtraParameters
+{
+    public class ExtraParametersHandler
+        : HttpClientHandler
+    {
+        private readonly Dictionary<string, string> _additionalParameters;
+
+        public ExtraParametersHandler(Dictionary<string, string> additionalParameters) =>
+            _additionalParameters = additionalParameters;
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            // TODO: add parameters to querystring
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+}
